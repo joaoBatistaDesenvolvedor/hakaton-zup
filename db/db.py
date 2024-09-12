4fc783154663e9a8f05f70f36444a576e0d7e8bc
Eduardo Silva <69769953+EduardoRS78@users.noreply.github.com>
2024-09-12 08:50:31 -0300

    login finalizado

--- db/db.py
+++ db/db.py
@@ -2,8 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# URL de conexão com o banco de dados PostgreSQL
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/meubanco"
 
+# Criação do engine para o banco de dados
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Criação da sessão local
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base para as classes de modelo
 Base = declarative_base()
+
+# Função para obter a sessão do banco de dados
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
